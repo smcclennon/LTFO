80ed36df4e83854fcad95cdd232a9f41befae080
Shiraz <24913281+smcclennon@users.noreply.github.com>
2019-10-12 14:51:16 +0100

    Build 1
    The first working build of Logout Reminder

---
+++ Logout Reminder.py
@@ -0,0 +1,149 @@
+#Logout Reminder
+build=1
+
+
+
+print('Importing requirements...')
+import time,string,os,socket,getpass
+from ctypes import windll
+from random import randint
+from pathlib import Path
+
+windll.kernel32.SetConsoleTitleW('Logout Reminder - Build '+str(build))
+
+def cmd(x):
+    os.system(str(x))
+def sleep(x):
+    time.sleep(x)
+def asciiRaw():
+    print('''  _                             _     _____                _           _
+ | |                           | |   |  __ \              (_)         | |
+ | |     ___   __ _  ___  _   _| |_  | |__) |___ _ __ ___  _ _ __   __| | ___ _ __
+ | |    / _ \ / _` |/ _ \| | | | __| |  _  // _ \ '_ ` _ \| | '_ \ / _` |/ _ \ '__|
+ | |___| (_) | (_| | (_) | |_| | |_  | | \ \  __/ | | | | | | | | | (_| |  __/ |
+ |______\___/ \__, |\___/ \__,_|\__| |_|  \_\___|_| |_| |_|_|_| |_|\__,_|\___|_|
+               __/ |''')
+    print('              |___/                                                    Build '+str(build)+'\n')
+def display():
+    cmd('cls')
+    asciiRaw()
+computer=str(socket.gethostname())
+username=str(getpass.getuser())
+driveArray = []
+bitmask = windll.kernel32.GetLogicalDrives()
+for letter in string.ascii_uppercase:
+    if bitmask & 1:
+        driveArray.append(letter)
+    bitmask >>= 1
+
+
+defaultMsg='You forgot to logout of '+computer+'!\nThis is a friendly reminder that you should probably do that next time.'
+
+
+def setupMessage():
+    display()
+    print('Computer: '+computer)
+    print('Username: '+username)
+    print('\nEnter your custom message: (leave blank to skip)')
+    global customMessage
+    customMessage=input(str('> '))
+    confirmMessage()
+
+def confirmMessage():
+    display()
+    global msg
+    if customMessage=='':
+        print('Custom Message: Disabled')
+        msg=defaultMsg
+        print('\n______ Message ______\n\n'+msg+'\n\n______ Message ______\n')
+    else:
+        print('Custom Message: Enabled')
+        msg=defaultMsg+'\n\nCustom Message:\n'+customMessage
+        print('\n______ Message ______\n\n'+msg+'\n\n______ Message ______\n')
+    confirm=input(str('\nConfirm? [Y/n] ')).upper()
+    if confirm=='Y':
+        setupDrive()
+    else:
+        setupMessage()
+
+def setupDrive():
+    display()
+    print('Available Drives:')
+    i=0
+    for x in driveArray:
+        i=i+1
+        print('{}. {}'.format(i,str(x))) #format what {} values are
+    print('\nPlease type the drive letter you wish to select (leave blank to go back)')
+    global selectedDrive
+    selectedDrive=input(str('> ')).upper()
+    if selectedDrive=='':
+        setupMessage()
+    if not selectedDrive in driveArray:
+        print('Specified drive not found. Please try again.')
+        sleep(0.5)
+        setupDrive()
+    else:
+        confirmDrive()
+
+def confirmDrive():
+    display()
+    print('Selected Drive: '+selectedDrive)
+    time.sleep(1)
+    confirm=input(str('Confirm? [Y/n] ')).upper()
+    if confirm=='Y':
+        confirmWrite()
+    else:
+        setupDrive()
+
+def confirmWrite():
+    display()
+    print('Computer: '+computer)
+    print('Username: '+username)
+    if customMessage=='':
+        print('Custom Message: Disabled')
+    else:
+        print('Custom Message: Enabled')
+    print('Selected Drive: '+selectedDrive)
+    print('\n______ Message ______\n'+msg+'\n______ Message ______\n')
+    time.sleep(1)
+    print('\nYou are about to flood all subdirectories in ['+selectedDrive+':\\]!')
+    time.sleep(0.5)
+    confirm=input(str('Are you sure? [Y/n] ')).upper()
+    if confirm=='Y':
+        commitWrite()
+    else:
+        setupDrive()
+
+def commitWrite():
+    rand=randint(10000,99999)
+    display()
+    print('\nSelected Drive: '+selectedDrive+'\n\nTo begin flooding, please type this confirmation code: '+str(rand))
+    confirm=input(str('\n>>> '))
+    if confirm!=str(rand):
+        confirmWrite()
+    removalMsg='\n\n\n\nInstructions to remove the files:\n1. Navigate to "'+selectedDrive+':\\"\n2. Search for "READ_ME ['+str(rand)+']"\n3. Select everything and delete'
+    global i
+    global start
+    i=0
+    print('\nCreating files...')
+    start=time.time()
+    for x in Path(selectedDrive+':/').glob('**'):
+        i=i+1
+        filename='READ_ME ['+str(rand)+'] [#'+str(i)+'].txt'
+        try:
+            f=open(str(x)+'\\'+filename, 'w')
+            f.write(msg+removalMsg)
+            f.close()
+            print(str(i)+'. '+str(x))
+        except:
+            print(str(i)+'. [FAILED] '+str(x))
+    
+    global taken
+    taken=time.time()-start
+
+
+setupMessage()
+print('\n')
+asciiRaw()
+print('Created '+str(i)+' files in '+str(round(taken, 2))+' seconds!\nPress any key to exit...')
+cmd('pause>nul')
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Logout Reminder
  A simple python script that reminds allows you to remind your colleagues to logout next time.
+
+This script will place a text file with a message of your choice within every folder (directory) on the selected drive of your choice.
+It creates and places a cleanup script so that the victim can easily clean up the mess.
+
+Written in Python 3.7.4 on Windows 10
